e2ebf21a67c7d66029858b62cd17caec3e644aff
adrisala20 <adriana_15_tuya@hotmail.com>
2024-04-02 15:15:59 +0200

    add objects in data.js

---
+++ js/data.js
@@ -0,0 +1,34 @@
+
+export const todo = [
+    {
+        id:1,
+        text: 'Sensazioni di pericolo o minaccia',
+        check: false
+    },
+    {
+        id:2,
+        text: 'Preoccupazioni per la salute, il futuro, per il lavoro o per le persone care',
+        check: false
+    },
+    {
+        id:3,
+        text:'Difficoltà di concentrazione',
+        check: false
+    },
+    {
+        id:4,
+        text:'Confusione',
+        check: false
+    },
+    {
+        id:5,
+        text:'Sensazione di affaticamento',
+        check: false
+    },
+    {
+        id: 6,
+        text:'Calo di efficienza nelle prestazioni',
+        check:false
+    }
+
+]
--- js/utility.js
+++
@@ -1,5 +0,0 @@
-
-// numeri random
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
